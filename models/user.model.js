69b4310fd51deb71e161a6821dec436571cd77d4
4-HrCoder <161428010+4-HrCoder@users.noreply.github.com>
2024-02-27 13:00:01 +0530

    Initial commit

---
+++ models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      lowecase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //aws,cloudaniry url
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Video',
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, 'Password is required!'],
+    },
+    referanceToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
